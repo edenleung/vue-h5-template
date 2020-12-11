4a332539b330e537cff61851c6dcaa7a9a0e8ef1
Eden Leung <31346973+edenleung@users.noreply.github.com>
2020-12-11 09:28:39 +0800

    指定开发人员开始debug

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import app_config from '@/app'
+import VConsole from 'vconsole'
 import '@/core/vant'
 
 // import './permission'
@@ -11,6 +12,14 @@ import '@/core/vant'
 import '@/utils/filter'
 
 Vue.config.productionTip = false
+
+if (app_config.debug && process.env.NODE_ENV === 'production') {
+  // 指定开发人者开启vconsole
+  const debuger = app_config.getStorage('openid')
+  if (app_config.debuger.lenght && debuger && app_config.debuger.indexOf(debuger) !== -1) {
+    new VConsole()
+  }
+}
 
 new Vue({
   router,
